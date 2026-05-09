6358a5e0a2886c19d6a210348b7555955790a100
orolik74 <orolik52@gmail.com>
2026-05-09 03:07:23 +0300

    initialize main pages and draft css and js logic

---
+++ Web/wwwroot/js/index.js
@@ -0,0 +1,37 @@
+const modal = document.getElementById("authModal");
+const loginBtn = document.getElementById("loginBtn");
+const closeBtn = document.querySelector(".close");
+const playBtn = document.getElementById("playBtn");
+
+loginBtn.onclick = () => {
+    modal.style.display = "block";
+}
+
+closeBtn.onclick = () => {
+    modal.style.display = "none";
+}
+
+window.onclick = (event) => {
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+}
+
+playBtn.onclick = () => {
+    window.location.href = 'room-list.html';
+}
+
+
+function openTab(evt, tabName) {
+    let i, tabContent, tabLinks;
+    tabContent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabContent.length; i++) {
+        tabContent[i].classList.remove("active");
+    }
+    tabLinks = document.getElementsByClassName("tab-link");
+    for (i = 0; i < tabLinks.length; i++) {
+        tabLinks[i].classList.remove("active");
+    }
+    document.getElementById(tabName).classList.add("active");
+    evt.currentTarget.classList.add("active");
+}
