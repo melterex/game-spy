63e7b750f49cf49fff3a142992eec8126c72692d
melterex <melt.t.aleksandrov@gmail.com>
2026-05-15 17:14:31 +0300

    Add GameLogic Interfaces

---
+++ GameLogic/Entities/GameSession.cs
@@ -0,0 +1,19 @@
+﻿using authorization;
+using GameLogic.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameLogic.Entities
+{
+    public abstract class GameSession
+    {
+        public Guid GameId { get; set; }
+        public List<UserId> playersIDs { get; set; }
+        public GameStage CurrentStage { get; set; }
+        public Int32 CurrentRound { get; set; }
+        public String CurrentWord { get; set; }
+        public List<Messages> MessagesList { get; set; }
+        public GameSettings GameSettings { get; set; }
+    }
+}
---
+++ GameLogic/Entities/Messages.cs
@@ -0,0 +1,14 @@
+﻿using authorization;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameLogic.Entities
+{
+    public class Messages
+    {
+        public UserId Id;
+
+        List<String> MessagesList;
+    }
+}
